b5b44150705dd215c1ca1f9c7f224c8ac22f847e
Athos Gustavo <athosgustavowf@gmail.com>
2025-04-26 20:46:14 -0300

    Tela de login

---
+++ app/src/main/java/projeto/piloto/projeto_off_web/Activity/LoginActivity.java
@@ -0,0 +1,85 @@
+package projeto.piloto.projeto_off_web.Activity;
+
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.google.android.material.snackbar.Snackbar;
+
+import projeto.piloto.projeto_off_web.Database.FirebaseAutenticacao;
+import projeto.piloto.projeto_off_web.R;
+import projeto.piloto.projeto_off_web.databinding.ActivityLoginBinding;
+
+public class LoginActivity extends AppCompatActivity {
+
+  private ActivityLoginBinding activityLoginBinding;
+  private FirebaseAutenticacao firebaseAutenticacaoAuth;
+  private boolean cadastro = false;
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+
+    activityLoginBinding = ActivityLoginBinding.inflate(getLayoutInflater());
+
+    EdgeToEdge.enable(this);
+    setContentView(activityLoginBinding.getRoot());
+    ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+      Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+      v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+      return insets;
+    });
+
+    clickAindaNaoTemConta();
+    configuraBtnEntrarCadastrar();
+    firebaseAutenticacaoAuth = new FirebaseAutenticacao(activityLoginBinding);
+
+  }
+
+  private void configuraBtnEntrarCadastrar() {
+    activityLoginBinding.btnEntrarCadastrarEntrar.setOnClickListener(new View.OnClickListener() {
+      @Override
+      public void onClick(View view) {
+        String inputConfirmarSenha = activityLoginBinding.inputConfirmarSenha.getText().toString();
+        String inputSenha = activityLoginBinding.inputSenha.getText().toString();
+        String inputEmail = activityLoginBinding.inputEmail.getText().toString();
+
+        try{
+          if (cadastro) {
+            firebaseAutenticacaoAuth.realizarCadastro(LoginActivity.this,inputEmail,inputSenha,inputConfirmarSenha);
+          } else {
+            firebaseAutenticacaoAuth.realizarLogin(LoginActivity.this,inputEmail,inputSenha);
+          }
+        }catch(IllegalArgumentException ex){
+          Snackbar.make(activityLoginBinding.getRoot(), "Atenção, os campos de e-mail e senha devem ser preenchidos", Snackbar.LENGTH_LONG).show();
+        }
+
+      }
+    });
+  }
+
+  private void clickAindaNaoTemConta() {
+    activityLoginBinding.textoMudaLoginCadastro.setOnClickListener(new View.OnClickListener() {
+      @Override
+      public void onClick(View view) {
+        cadastro = !cadastro;
+        if (cadastro) {
+          activityLoginBinding.inputConfirmarSenha.setVisibility(View.VISIBLE);
+          activityLoginBinding.btnEntrarCadastrarEntrar.setText("Cadastrar");
+          activityLoginBinding.textoMudaLoginCadastro.setText("Já tem uma conta ?");
+        } else {
+          activityLoginBinding.inputConfirmarSenha.setVisibility(View.GONE);
+          activityLoginBinding.btnEntrarCadastrarEntrar.setText("Entrar");
+          activityLoginBinding.textoMudaLoginCadastro.setText("Ainda não tem uma conta ?");
+        }
+      }
+    });
+  }
+
+
+}
---
+++ app/src/main/java/projeto/piloto/projeto_off_web/Database/FirebaseAutenticacao.java
@@ -0,0 +1,81 @@
+package projeto.piloto.projeto_off_web.Database;
+
+import android.app.Activity;
+
+import androidx.annotation.NonNull;
+import androidx.viewbinding.ViewBinding;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.android.material.snackbar.Snackbar;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseAuthInvalidCredentialsException;
+import com.google.firebase.auth.FirebaseAuthUserCollisionException;
+import com.google.firebase.auth.FirebaseAuthWeakPasswordException;
+
+public class FirebaseAutenticacao {
+
+  private ViewBinding viewBinding;
+  private final FirebaseAuth firebaseAuth = FirebaseAuth.getInstance();
+
+  public FirebaseAutenticacao(ViewBinding viewBinding){
+    this.viewBinding = viewBinding;
+  }
+
+  public void realizarCadastro(Activity activity, String inputEmail, String inputSenha, String inputConfirmarSenha) {
+
+    if (inputConfirmarSenha.equals(inputSenha)) {
+      firebaseAuth.createUserWithEmailAndPassword(inputEmail, inputSenha)
+              .addOnCompleteListener(activity, new OnCompleteListener<AuthResult>() {
+                @Override
+                public void onComplete(@NonNull Task<AuthResult> resposta) {
+                  if(resposta.isSuccessful()){
+                    Snackbar.make(viewBinding.getRoot(), "Conta criada com sucesso !.", Snackbar.LENGTH_LONG).show();
+                  }else{
+                    tratarRespostaAutenticacao(resposta);
+                  }
+
+                }
+              });
+    } else {
+      Snackbar.make(this.viewBinding.getRoot(), "As senhas digitadas não são iguais.", Snackbar.LENGTH_LONG).show();
+    }
+
+  }
+
+  public void tratarRespostaAutenticacao(Task<AuthResult> resposta) {
+    if (!resposta.isSuccessful()) {
+      try {
+        throw resposta.getException();
+      } catch (FirebaseAuthWeakPasswordException e) {
+        Snackbar.make(this.viewBinding.getRoot(), "Senha fraca.A senha deve conter no mínimo 6 caracteres.", Snackbar.LENGTH_LONG).show();
+      } catch (FirebaseAuthInvalidCredentialsException e) {
+        if(e.getErrorCode().equals("ERROR_INVALID_EMAIL")){
+          Snackbar.make(this.viewBinding.getRoot(), "E-mail formatado incorretamente.Use o formato nome@dominio.com", Snackbar.LENGTH_LONG).show();
+        }
+        if(e.getErrorCode().equals("ERROR_INVALID_CREDENTIAL")){
+          Snackbar.make(this.viewBinding.getRoot(), "A senha ou e-mail estão incorretos.", Snackbar.LENGTH_LONG).show();
+        }
+      } catch (FirebaseAuthUserCollisionException e) {
+        Snackbar.make(this.viewBinding.getRoot(), "O e-mail já está vinculado a outra conta.", Snackbar.LENGTH_LONG).show();
+      } catch (Exception e) {
+        Snackbar.make(this.viewBinding.getRoot(), "Erro inesperado, contate o administrador.", Snackbar.LENGTH_LONG).show();
+      }
+    }
+  }
+
+  public void realizarLogin(Activity activity,String email, String senha) {
+    firebaseAuth.signInWithEmailAndPassword(email, senha)
+            .addOnCompleteListener(activity,new OnCompleteListener<AuthResult>() {
+              @Override
+              public void onComplete(@NonNull Task<AuthResult> resposta) {
+                if(resposta.isSuccessful()){
+                  Snackbar.make(viewBinding.getRoot(), "Seja bem vindo.", Snackbar.LENGTH_LONG).show();
+                }else{
+                  tratarRespostaAutenticacao(resposta);
+                }
+              }
+            });
+  }
+}
